70689f18a7ecbc875749c2e24ec92e4162f5b21a
djlyon <david.lyon@hackerpads.com>
2014-06-18 19:32:39 +1000

    Authentication cleanup

---
+++ install_files/pi-package-install.sh
@@ -0,0 +1,2 @@
+apt-get install libusb-dev libltdl-dev libghoto2-2 gvfs-bin python-pip
+pip install pysftp
--- pi-package-install.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get install libusb-dev libltdl-dev libghoto2-2 gvfs-bin
-sudo easy_install pysftp
