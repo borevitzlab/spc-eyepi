074c11d89dc74479023dd4cd79791133efc10edd
djlyon <david.lyon@hackerpads.com>
2014-07-09 06:28:18 +1000

    Resolved Issue #10

--- install_files/pi-package-install.sh
+++ install_files/pi-package-install.sh
@@ -1,3 +1,9 @@
-apt-get install libusb-dev libltdl-dev libghoto2-2 gvfs-bin python-pip
+apt-get install autotools-dev cmake gvfs-bin ipython ipython-notebook
+apt-get install libexpat1-dev libltdl-dev libssl-dev libtool libusb-dev
+apt-get install python-dev python-pip python-virtualenv virtualenvwrapper
+apt-get install libgmp-dev vim tree ntpdate libjpeg8-dev liblcms2-dev
+apt-get install imagemagick-common imagemagick ufraw-batch dcraw
+apt-get install htop python-usb apt-get install libusb-dev libltdl-dev 
+apt-get install libghoto2-2 gvfs-bin python-pip
 pip install pysftp
 gcc -o dcraw -O4 dcraw.c -lm -ljpeg -llcms2 -DNO_JASPER
