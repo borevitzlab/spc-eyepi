67f040e3848aa51861aeea087880a438b1c3e09e
clixx-io <david.lyon@hackerpads.com>
2014-07-11 12:20:02 +1000

    Initial checkin of web interface

---
+++ webinterface.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<img src=' + url_for('static',filename='last_image.jpg') + '>' 
+      
+if __name__ == "__main__":
+    app.run()     
