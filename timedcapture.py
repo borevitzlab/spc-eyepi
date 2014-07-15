f9fb33a49ea3e197b3a7ddace83d91a9199ec6a9
djlyon <david.lyon@hackerpads.com>
2014-07-16 08:16:57 +1000

    Fix syntax error

--- timedcapture.py
+++ timedcapture.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
                     # Save the jpeg to the web servers directory
                     for i in converted_files:
                         if i.endswith('.jpg'):
-                            shutil.copy_file(i,os.path.join('static',os.path.basename(i)))
+                            shutil.copy(i,os.path.join('static',os.path.basename(i)))
 
                 except Exception, e:
                     logger.error("Image Capture error - " + str(e))
