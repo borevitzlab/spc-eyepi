ed9c468a31f3ee7da2e5056a0bf1ab422606582a
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2014-11-26 07:02:03 +0000

    auth for webinterface

--- webinterface.py
+++ webinterface.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-import socket, os
-from flask import Flask, redirect, url_for, request
+import socket, os, hashlib
+import Crypto.Protocol.KDF
+import anydbm
+from functools import wraps
+from flask import Flask, redirect, url_for, request, send_file, abort, Response
 from ConfigParser import SafeConfigParser
 
 config_filename = 'eyepi.ini'
@@ -10,14 +13,41 @@ example_filename = 'example.ini'
 app = Flask(__name__)
 app.debug = True
 
+def sanitizeconfig(towriteconfig):
+    print "do checking here"
+    with open(config_filename, 'wb') as configfile:
+        towriteconfig.write(configfile)
+        
+def check_auth(username, password):
+    db = anydbm.open('db', 'r')
+    m = Crypto.Protocol.KDF.PBKDF2(password=str(password),salt=str(username),count=10000)
+    if m==db[username]:
+        return True
+        db.close()
+    db.close()
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth=request.authorization
+        if not auth or not check_auth(auth.username,auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+        
+def authenticate():
+    return Response('Access DENIED!, Please log in:',401,{'WWW-Authenticate':'Basic realm="Login Required"'})
+
+
 @app.route('/restart')
 def restart():
+            #
     print "shutting down"
     os.system("reboot")
     return redirect(url_for('index'))
 
 def createform(position, configfile, example):
-    returnstring = "<div style='border:2px solid;border-radius:3px;width:49%;float:"+position+";'>"
+    returnstring = "<script type='text/javascript'> function sa(s){var d=document.getElementById('h'+s);d.style.display=d.style.display!='none'?'none':'block';};</script> <div style='padding:10px;border:2px solid;border-radius:3px;width:47%;float:"+position+";'>"
     returnstring +="<h3>"+configfile +"</h3>"
     if position == "left":returnstring += "<a href="+ url_for('lastimage')+">"+"LAST IMAGE</a>"
     else: returnstring += "<a href="+ url_for('lastpicam')+">"+"LAST IMAGE</a>"
@@ -26,7 +56,11 @@ def createform(position, configfile, example):
     config = SafeConfigParser()
     config.read(configfile)
     for section in config.sections():
-        returnstring += "<div><h4>"+section+"</h4>"
+        if section == "mqtt":
+            returnstring += "<br><button onclick=\"sa('"+position+"');return false;\">Advanced options</button>"
+            returnstring += "<div id='h"+position+"' style='display:none;'><h4>"+section+"</h4>"
+        else:
+            returnstring += "<div><h4>"+section+"</h4>"
         for option in config.options(section):
             try:
                 if option == "enabled" or option == "uploadwebcam" or option == "uploadtimestamped" or option == "uploaderenabled":
@@ -39,16 +73,19 @@ def createform(position, configfile, example):
                     returnstring += option + "<input type='text' title=example:"+example.get(section,option)+" name="+section+'.'+option+" placeholder="+config.get(section, option)+" ><br>"
             except Exception as e:
                 returnstring += option + "<input type='text' placeholder='NO!' >"
+            
             #strsect = str(section)
             #print strsect+","+strit
             #opti = config.get(strsect,strit)
 	    #print opti
-        returnstring += "</div>"
-    returnstring += "</form></div>"
+        if section != "mqtt":
+            returnstring += "</div>"
+    returnstring += "</div></form></div>"
     return returnstring
 
 @app.route('/right', methods=['GET','POST'])
 def right():
+    
     aconfig = SafeConfigParser()
     aconfig.read(otherconfig_filename)
     aconfig.set("camera","enabled","off")
@@ -63,10 +100,11 @@ def right():
                 opt = key.split(".")[1]
                 aconfig.set(sect,opt,value)
                 print "changed: "+sect+':'+opt+':'+value
-        with open(otherconfig_filename, 'wb') as configfile:
-            aconfig.write(configfile)
-    print "returning right"
-    return redirect(url_for('index'))
+        try:
+            sanitizeconfig(aconfig)
+            return redirect(url_for('index'))
+        except Exception as e:
+            abort(400)
 
 @app.route('/left', methods=['GET','POST'])
 def left():
@@ -80,20 +118,22 @@ def left():
         for key, value in request.form.iteritems(multi=True):
             if value != "":
                 sect = key.split('.')[0]
-                opt = key.split(".")[1]
+                opt = key.split(".")[1] 
                 aconfig.set(sect,opt,value)
                 print "changed: "+sect+':'+opt+':'+value
-        with open(config_filename, 'wb') as configfile:
-            aconfig.write(configfile)
-    print "returning left"
-    return redirect(url_for('index'))
+        try:
+            sanitizeconfig(aconfig)
+            return redirect(url_for('index'))
+        except Exception as e:
+            abort(400)
 
     
 @app.route('/', methods=['GET','POST'])
+@requires_auth
 def index():
     example = SafeConfigParser()
     example.read(example_filename)
-    returnstring = "<html><body><h1>Configuration Page for "+socket.gethostname()+"</h1><form action=restart><button>REBOOT</button></form><br>"
+    returnstring = "<html><body><h1><marquee behaviour='alternate'>Configuration Page for "+socket.gethostname()+"</marquee></h1><form action=restart><button>REBOOT</button></form><br>"
     returnstring += createform("left", config_filename, example)
     returnstring += createform("right", otherconfig_filename, example)
     return returnstring
@@ -102,14 +142,28 @@ def index():
 def lastimage():
     config = SafeConfigParser()
     config.read(config_filename)
-    return '<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul, 2002 12:00:00 GMT"><script type="text/javascript">window.setTimeout(function(){document.location.reload(true);},'+str(float(config.get("timelapse","interval"))*1000)+');</script><img src="'+url_for('static',filename='dslr_last_image.jpg')+"></div>'
+    return '<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul, 2002 12:00:00 GMT"><html><script type="text/javascript">window.setTimeout(function(){document.location.reload(true);},'+str(float(config.get("timelapse","interval"))*1000)+');</script><body><img src='+ url_for("static",filename="dslr_last_image.jpg")+'></img></body></html>'
 
+@app.route("/dslr_last_image.jpg")
+def DSLR_last_image():
+    if os.path.isfile("static/dslr_last_image.jpg"):
+        return send_file("static/dslr_last_image.jpg")
+    else:
+        abort(404)
+
+@app.route("/pi_last_image.jpg")
+def PI_last_image():
+    if os.path.isfile("static/pi_last_image.jpg"):
+        return send_file("static/pi_last_image.jpg")
+    else:
+        abort(404)
 
 @app.route("/lastpicam")
 def lastpicam():
     config = SafeConfigParser()
     config.read(otherconfig_filename)
-    return '<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul, 2002 12:00:00 GMT"><script type="text/javascript">window.setTimeout(function(){document.location.reload(true);},'+str(float(config.get("timelapse","interval"))*1000)+');</script><img src="'+url_for('static',filename='pi_last_image.jpg')+"></div>'
+    return '<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul, 2002 12:00:00 GMT"><script type="text/javascript">window.setTimeout(function(){document.location.reload(true);},'+str(float(config.get("timelapse","interval"))*1000)+');</script><img src="' + url_for("static",filename="pi_last_image.jpg")+'></div>'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
