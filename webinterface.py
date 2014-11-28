7b9a88978c0821bbe88d83ce852c3f7367665641
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2014-11-28 03:42:26 +0000

    made rotate log button rotate the log to the upload directories

--- webinterface.py
+++ webinterface.py
@@ -2,6 +2,7 @@
 import socket, os, hashlib, subprocess
 import Crypto.Protocol.KDF
 import anydbm
+import datetime
 from functools import wraps
 from flask import Flask, redirect, url_for, request, send_file, abort, Response
 from ConfigParser import SafeConfigParser
@@ -52,7 +53,16 @@ def authenticate():
 @app.route('/rotatelogfile')
 @requires_auth
 def rotatelogfile():
-    #do log file rotation here
+    config = SafeConfigParser()
+    config.read(config_filename)
+    config2 = SafeConfigParser()
+    config2.read(otherconfig_filename)
+    with open("static/logfile.txt",'r') as log:
+        with open(os.path.join(config.get("copying","directory"),datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')+".log"),'w' ) as writeout:
+            writeout.write(log.read())
+        with open(os.path.join(config2.get("copying","directory"),datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')+".log"),'w' ) as writeout:
+            writeout.write(log.read())
+                  
     open("static/logfile.txt","w").close()
     return redirect(url_for('logfile'))
 
