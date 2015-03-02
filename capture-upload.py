edcf30ba4027a6869f69fd926502640e6064d8e2
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2015-03-02 16:36:12 +1100

    more sensical eroor checking from gphoto2

--- capture-upload.py
+++ capture-upload.py
@@ -193,10 +193,12 @@ class Camera(Thread):
                     #    #cmd = ["gphoto2 --set-config capturetarget=sdram --capture-image-and-download --filename='"+os.path.join(self.spool_directory, os.path.splitext(raw_image)[0])+".%C'"]
                     # subprocess.call. shell=True is hellishly insecure and doesn't throw an error if it fails. Needs to be fixed somehow <shrug>
                     try:
-                        output = subprocess.check_output(cmd,stderr=subprocess.STDOUT, shell=True)
+                        output = subprocess.check_output(cmd,stderr=subprocess.STDERR, shell=True)
                     except subprocess.CalledProcessError as e:
                         self.logger.error("Something went wrong!!! %s "% str(e))
-                    self.logger.debug("GPHOTO2: "+ output)
+                    for line in output.splitlines():
+                        self.logger.debug("GPHOTO2: "+ line)
+
                     self.logger.debug("Capture Complete")
                     self.logger.debug("Moving and renaming image files, buddy")
 
