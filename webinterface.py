c916e32a6ba3cfbda77dd37a2f7fb39aa280041e
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2015-10-28 15:50:24 +1100

    V3.1!!! TESTED AND FUNCTIONAL.

--- webinterface.py
+++ webinterface.py
@@ -384,7 +384,7 @@ def restart():
 @requires_auth
 def update():
     os.system("git fetch --all")
-    os.system("git reset --hard origin/master")
+    os.system("git reset --hard origin/python3")
     return "SUCCESS"
 
 
