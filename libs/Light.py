df1b362a813ca1bebc173f1c8f9d4ed663c234dd
Gareth Dunstone <freedom.2.the.leetle.people@gmail.com>
2018-01-31 15:50:26 +1100

    adding PSI light module

--- libs/Light.py
+++ libs/Light.py
@@ -7,6 +7,7 @@ import time
 from telnetlib import Telnet
 import json
 import requests
+import serial
 
 try:
     logging.config.fileConfig("logging.ini")
@@ -63,7 +64,7 @@ class Controller(object):
         if not self.set_all_command:
             self.logger.error("set_all call without set_all_command")
             return None
-        if percent:
+        if percent is not None:
             power = int(self.max * (percent / 100) + self.min)
         cmd = None
         if "{power}" in self.set_all_command:
@@ -71,11 +72,11 @@ class Controller(object):
         elif "{percent}" in self.set_all_command:
             cmd = self.set_all_command.format(percent=percent)
         else:
-            self.logger.error("set_wavelength no cmd")
+            self.logger.error("set_one no cmd")
             return None
         return self._run_command(cmd)
 
-    def set_wavelength(self, wl: str, power: int = None, percent: int = None):
+    def set_one(self, wl: str, power: int = None, percent: int = None):
         """
         sets a specific wavelength to a value
         either a power or a percent must be specified
@@ -85,9 +86,9 @@ class Controller(object):
         :param percent: percent value, calculated from min/max.
         """
         if not self.set_wavelength_command:
-            self.logger.error("set_wavelength call without set_wavelength_command")
+            self.logger.error("set_one call without set_wavelength_command")
             return None
-        if percent:
+        if percent is not None:
             power = int(self.max * (percent / 100) + self.min)
         cmd = None
         if "{power}" in self.set_wavelength_command:
@@ -95,11 +96,11 @@ class Controller(object):
         elif "{percent}" in self.set_wavelength_command:
             cmd = self.set_wavelength_command.format(wavelength=wl, percent=percent)
         else:
-            self.logger.error("set_wavelength no cmd")
+            self.logger.error("set_one no cmd")
             return None
         return self._run_command(cmd)
 
-    def set_all_wavelengths(self, values: dict, percent=True):
+    def set_all_each(self, values: dict, percent=True):
         """
         sets all wavelengths to specific values.
         only absolute values may be specified
@@ -110,7 +111,7 @@ class Controller(object):
         """
 
         if not self.set_all_wavelength_command:
-            self.logger.error("set_all_wavelengths call without set_all_wavelength_command")
+            self.logger.error("set_all_each call without set_all_wavelength_command")
             return None
         r = re.compile(r'\d+')
 
@@ -119,11 +120,11 @@ class Controller(object):
             return 0 if not vs else int(vs.group())
 
         sorted_values = sorted(values.items(), key=keygetter)
-        if percent:
+        if percent is not None:
             sorted_values = [(k, int(self.max * (v / 100) + self.min)) for k, v in sorted_values]
 
         if len(values) < self.set_all_wavelength_command.count("{}"):
-            self.logger.error("Not enough wavelengths specified for set_all_wavelengths, padding with 0s")
+            self.logger.error("Not enough wavelengths specified for set_all_each, padding with 0s")
             diff = self.set_all_wavelength_command.count("{}") - len(values)
             sorted_values.extend(("padded", 0) for _ in range(diff))
         sorted_values = [(k, clamp(v, self.min, self.max)) for k, v in sorted_values]
@@ -132,14 +133,14 @@ class Controller(object):
             return dict([(str(k).lower(), int(v)) for k, v in sorted_values])
         return {}
 
-    def get_wavelength(self, wavelength: str):
+    def get_one(self, wavelength: str):
         """
         gets the power of a specific wavelength
         :param wavelength: wavelength string to get the power of (eg 400nm)
         :return:
         """
         if not self.get_wavelength_command:
-            self.logger.error("get_wavelength call without get_wavelength_command")
+            self.logger.error("get_one call without get_wavelength_command")
             return None
 
         cmd = None
@@ -186,6 +187,130 @@ class TelNetController(Controller):
             return False
         finally:
             telnet.close()
+
+
+class PSISerialInterfaceController(Controller):
+    """
+    Controller for Photon Systems Instruments lights.
+    """
+
+    def __init__(self, address, available_channels=(0, 1, 3, 4, 5, 6, 7, 8)):
+        super(PSISerialInterfaceController, self).__init__({})
+        self.available_channels = available_channels
+        self.address = address
+
+        for x in range(5):
+            try:
+                self.ser = serial.Serial('/dev/ttyUSB{}'.format(x), 9600, bytesize=serial.EIGHTBITS,
+                              parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+                              rtscts=False, dsrdtr=False, xonxoff=False)
+                break
+            except:
+                traceback.print_exc()
+                time.sleep(0.5)
+                continue
+        self.max = 1022
+        self.min = 1000
+
+    def __del__(self):
+        try:
+            self.ser.close()
+        except:
+            traceback.print_exc()
+            pass
+
+    def construct_packet(self, channel: int,
+                         value: int,
+                         operation: int = 0,
+                         t: int = 1) -> bytearray:
+        """
+        Constructs a packet according to the protocol laid out by PSI to set a channel to value. G.
+
+        :param channel: channel to set
+        :param value: value between 0-1022
+        :param operation: operation, should be 0 for set.
+        :param t: type, apparently should be 1 for us in psi spec
+        :return: bytearray fo the packet
+        """
+        if channel not in self.available_channels:
+            pass
+            # raise ValueError("Channel < {} > is not in available channels 0 ,1 ,3 ,4, 5, 6, 7, 8".format(channel))
+
+        if not (0 <= value < 1022):
+            raise ValueError("Value < {} > is not within range 0-1022 specified for PSI lights".format(value))
+
+        # header - always ST P.
+        packet = [ord('S'), ord('T'), 0, 0, 0, 0, 0]
+
+        # target + nibble of address P.
+        packet[2] = (t << 4) | ((self.address >> 8) & 0x0F)
+        # the rest of the address P.
+        packet[3] = self.address & 0xFF
+
+        # opcode + payload P.
+        # opcode = 4b channel + 2b instr P.
+        # is 'instr' meant to be operation? G.
+        # either way this should now be working... G.
+        packet[4] = (((channel << 4) & 0xF0) | ((operation << 2) & 0x0C) | ((value >> 8) & 0x03))
+        packet[5] = value & 0xFF
+        # checksum for last byte of packet.
+        chk = 0
+        for byt in packet[:6]:
+            chk ^= byt
+        packet[6] = chk & 0xFF
+        return bytearray(packet)
+
+    def activate(self, c):
+        """
+        helper to get an activation packet.
+        :param c:
+        :return:
+        """
+        activate_packet = self.construct_packet(c, 1, operation=2)
+        activate_packet += self.construct_packet(c, 1, operation=3)
+        self.ser.write(activate_packet)
+
+    def set_one(self, c: int, power: int = None, percent: int = None):
+        """
+        set one channel re the psi "spec"
+
+        :param c: channel number
+        :param power: power in 0-1022
+        :param percent: power percentage (0-100)
+        """
+        if percent is not None:
+            power = int(self.max * (percent / 100) + self.min)
+        # actvate the channel for light
+        self.activate(c)
+        intensity_packet = self.construct_packet(c, power, operation=0)
+        intensity_packet += self.construct_packet(c, power, operation=1)
+        self.ser.write(intensity_packet)
+
+    def set_all(self, power: int = None, percent: int = None):
+        for c in self.available_channels:
+            self.set_one(c, power=power)
+        return True
+
+    def set_all_each(self, values: dict, percent=True):
+        """
+        sets all wavelengths to specific values.
+        only absolute values may be specified
+        values should be specified as a dict of wavelength: value pairs
+
+        :param values: dict of channels and their respective values
+        :param percent: whether the values are expressed as 0-100 or absolute.
+        """
+        try:
+            if percent:
+                for c, v in values.items():
+                    self.set_one(c, percent=v)
+            else:
+                for c, v in values.items():
+                    self.set_one(c, power=v)
+            return dict(("chan-{}".format(k), v) for k, v in values.items())
+        except:
+            traceback.print_exc()
+            return {}
 
 
 class HTTPController(Controller):
@@ -238,6 +363,53 @@ class HTTPController(Controller):
             return False
 
 
+class PSILight(object):
+    """
+    runner for PSI serial lights
+    """
+    accuracy = 3
+    default_available_channels = (0, 1, 3, 4, 5, 6, 7, 8)
+
+    def __init__(self, config):
+        self.name = config.get("name")
+
+        self.logger = logging.getLogger(self.name)
+        self.logger.info("PSI light init...")
+        self.config = config.copy()
+        self.failed = list()
+
+        self.percent = config.get("percent", True)
+
+        self.min = 0
+        # 1022 max value as per psi spec.
+        self.max = 1022
+
+        self.address = int(self.config.get("address", 1))
+        self.controller = PSISerialInterfaceController(self.address)
+
+        self.available_channels = self.config.get("available_channels",
+                                           self.default_available_channels)
+
+    def set(self, intensities: list) -> dict:
+        """
+        sets the lights channels to the values in the list.
+
+        returns a dict of the channels set and their values.
+
+        If the length of the list of intensities doesnt match the number of custom wavelengths, but does match the
+        length of the list of s10 or s20 wavelengths, then they will be used.
+
+        If none of those conditions are met, returns an empty dict.
+
+        :param intensities: ordered list of intensities to set
+        :return: dict of set values
+        """
+        intensities = list(map(float, intensities))
+
+        values = dict(zip(self.available_channels, intensities))
+
+        return self.controller.set_all_each(values, percent=True)
+
 class HelioSpectra(object):
     """
     Dumb runnner for a light, must be controlled by a chamber.
@@ -254,7 +426,7 @@ class HelioSpectra(object):
         self.name = config.get("name")
 
         self.logger = logging.getLogger(self.name)
-        self.logger.info("init...")
+        self.logger.info("Helio init...")
         self.config = config.copy()
         self.failed = list()
 
@@ -302,4 +474,4 @@ class HelioSpectra(object):
                 return {}
         print("Setting light values: {}".format(str(values)))
 
-        return self.controller.set_all_wavelengths(values, percent=True)
+        return self.controller.set_all_each(values, percent=True)
